7f9a46818a09f850c0e8689f37b2fa56dde43e38
michelle <njerimwanjiku@gmail.com>
2020-08-26 01:14:01 +0300

    variales added

--- training.js
+++ training.js
@@ -1,2 +1,11 @@
-console.log(Vanta);
+console.log("Vanta");
+let VantaAge= 23;
+console.log(VantaAge);
+let myAge = 24; 
+let mySisterAge = 35; 
+let myBrotherName = "Jack";
+console.log(myAge, mySisterAge, myBrotherName);
+let WaterIsClear = false;
+let GodIsGood = true;
+console.log(WaterIsClear, GodIsGood);
 
