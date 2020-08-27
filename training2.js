fd866cbed53953d21270a2a8be1b44ed64adad56
michelle <njerimwanjiku@gmail.com>
2020-08-28 00:16:31 +0300

    Comparions JS

--- training2.js
+++ training2.js
@@ -20,3 +20,32 @@ let decrementNumber = 20
 decrementNumber--;
 console.log(decrementNumber);
 
+let myfirstNumber = 20;
+let mythirdNumber = 50;
+mythirdNumber = myfirstNumber;
+console.log(mythirdNumber);
+
+let Tom = 4;
+let Greg = 5;
+Greg == Tom;
+console.log (Greg == Tom);
+
+let Tiffanny = 6;
+let Joan = 6;
+Tiffanny == Joan;
+console.log(Tiffanny == Joan);
+
+let church = 7;
+let mosque = 8;
+
+church === mosque;
+console.log(church === mosque);
+
+let book = "8";
+let pen = 8;
+book === pen 
+console.log(book === pen);
+
+
+
+
